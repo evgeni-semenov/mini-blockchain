59ec9d618c2afbe115af5e0717b4d97a6f7276d4
Evgeni Semenov <84712585+evgeni-semenov@users.noreply.github.com>
2023-03-06 11:17:44 +0200

    Update demo.py
    added ability to set the network difficulty

--- demo.py
+++ demo.py
@@ -1,11 +1,13 @@
 from blockchain import Blockchain
 
+difficulty = 6 #you can try to modify the difficulty (amount of prefix zeros) to see how it affects the calculation time
+
 block_one_transactions = {"sender":"Alice", "receiver": "Bob", "amount":"50"}
 block_two_transactions = {"sender": "Bob", "receiver":"Cole", "amount":"25"}
 block_three_transactions = {"sender":"Alice", "receiver":"Cole", "amount":"35"}
 
 local_blockchain = Blockchain()
-local_blockchain.add_block(block_one_transactions)
-local_blockchain.add_block(block_two_transactions)
-local_blockchain.add_block(block_three_transactions)
+local_blockchain.add_block(block_one_transactions, difficulty)
+local_blockchain.add_block(block_two_transactions, difficulty)
+local_blockchain.add_block(block_three_transactions, difficulty)
 local_blockchain.print_blocks()
